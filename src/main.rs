f157372996f57d912ffd4e3a4c2346d40048b6a3
Jonathan Reicher <yony252525@gmail.com>
2026-01-28 17:45:08 +0200

    WIP

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,21 @@
-use lens_sl::{Register, Word64, inst, optimize};
+use lens_sl::{Register, Word64, Word8, Word4, inst, optimize};
 
 fn main() {
-    let p = optimize::<Word64, Word64>(
+    let p = optimize::<Word64, Word4>(
         &[
             inst!(AddI, 0, 1, 5),
             inst!(AddI, 0, 0, 5),
             inst!(AddI Eq, 1, 0, 1),
+            inst!(Mul Eq, 1, 0, 1),
+            inst!(Orr, 0, 0, 1),
+            inst!(AddI Eq, 1, 0, 1),
+            inst!(Eor, 1, 0, 1),
         ],
         &[
             &[(Register(0), 0), (Register(1), 10)],
             &[(Register(0), 0), (Register(1), 20)],
+            &[(Register(0), 1), (Register(1), 10)],
+            &[(Register(0), 10), (Register(1), 20)],
         ],
     );
     println!("Optimized program:");
