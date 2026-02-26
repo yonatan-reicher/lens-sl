6128fa0e6a5f2e8e44816192f6ef140ea422f882
Jonathan Reicher <yony252525@gmail.com>
2026-02-26 08:03:03 -0500

    suppress warnings

--- src/oracle.rs
+++ src/oracle.rs
@@ -5,8 +5,10 @@ pub trait Oracle<P, S> where P: ?Sized {
     fn check_program(&mut self, program: &P) -> Result<(), CounterExample<S>>;
 }
 
+#[allow(dead_code)]
 pub mod test_cases;
 pub mod smt;
 
+#[allow(unused_imports)]
 pub use test_cases::TestCasesOracle;
 pub use smt::SmtOracle;
