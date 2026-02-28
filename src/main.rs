6281858263fe0bf9cfc467df4f972f238fd49673
Jonathan Reicher <yony252525@gmail.com>
2026-02-28 10:42:06 -0500

    Use debug printer in main

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use lens_sl::DebugPrinterImpl;
 #[allow(unused_imports)]
 use lens_sl::{Register, Word64, Word8, Word4, inst, optimize};
 
@@ -20,6 +21,7 @@ fn main() {
             &[(Register(0), 93), (Register(1), 1)],
             &[(Register(0), 92), (Register(1), 11)],
         ],
+        &DebugPrinterImpl::default(),
     );
     let Some(p) = p else {
         println!("No equivalent program found");
