adc118a8394fcf3bdabf19f590cab199502abb23
Jonathan Reicher <yony252525@gmail.com>
2026-03-15 13:49:27 -0400

    bugfix for len trait
     now uses faster is_empty impls

--- src/len.rs
+++ src/len.rs
@@ -6,15 +6,36 @@ pub trait Len {
 }
 
 // Implement for references
-use std::rc::Rc;
-#[rustfmt::skip] impl<T: Len> Len for &T {     fn len(&self) -> usize{ T::len(self) } }
-#[rustfmt::skip] impl<T: Len> Len for &mut T { fn len(&self) -> usize{ T::len(self) } }
-#[rustfmt::skip] impl<T: Len> Len for Rc<T> { fn len(&self) -> usize{ T::len(self) } }
+macro_rules! ref_impl {
+    ($t:ty) => {
+        #[rustfmt::skip]
+        impl<T: Len> Len for $t {
+            fn len(&self) -> usize     { T::len(self) }
+            fn is_empty(&self) -> bool { T::is_empty(self) }
+        }
+    };
+}
+ref_impl!(&T);
+ref_impl!(&mut T);
+ref_impl!(std::rc::Rc<T>);
 
 // Implement for common types
-use rustc_hash::FxHashMap;
-use std::collections::HashMap;
-#[rustfmt::skip] impl<T> Len for [T] {                fn len(&self) -> usize { self.len() } }
-#[rustfmt::skip] impl<T> Len for Vec<T> {             fn len(&self) -> usize { self.len() } }
-#[rustfmt::skip] impl<K, V> Len for HashMap<K, V> {   fn len(&self) -> usize { self.len() } }
-#[rustfmt::skip] impl<K, V> Len for FxHashMap<K, V> { fn len(&self) -> usize { self.len() } }
+macro_rules! common_impl {
+    (<$($p:ident),*> $t:ty) => {
+        #[rustfmt::skip]
+        impl<$($p),*> Len for $t {
+            fn len(&self) -> usize     { self.len() }
+            fn is_empty(&self) -> bool { self.is_empty() }
+        }
+    };
+    ($t:ty) => {
+        #[rustfmt::skip]
+        impl<T> Len for $t {
+            fn len(&self) -> usize     { self.len() }
+            fn is_empty(&self) -> bool { self.is_empty() }
+        }
+    };
+}
+common_impl!([T]);
+common_impl!(Vec<T>);
+common_impl!(<K, V, S> std::collections::HashMap<K, V, S>);
