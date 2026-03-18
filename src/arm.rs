4c44e296bc02c9d68eb1622c080b47ebd8f18911
Jonathan Reicher <yony252525@gmail.com>
2026-03-18 03:22:10 -0400

    move state things to a new sub-module

--- src/arm.rs
+++ src/arm.rs
@@ -3,18 +3,14 @@
 use crate::all::All;
 use crate::all_permutations::Iter as PermutationIter;
 use crate::bool::prelude::*;
-use crate::collect_registers;
 use crate::enumerate::{EnumerationInfo, EnumerationInfoOptions, Enumerator};
 use crate::iter_slice_or_single::Iter as SliceOrSingle;
-use crate::oracle;
 use crate::reduce_bit_width::{ImmediateInfo, Reducer};
 use crate::word::prelude::*;
 
-use std::fmt::Debug;
+use std::fmt::{self, Debug, Display, Formatter};
 use std::ops::ControlFlow;
 
-// Derive macros
-use derive_more::Display;
 use serde::{Deserialize, Serialize};
 
 #[cfg(test)]
@@ -22,9 +18,7 @@ use proptest::prelude::*;
 
 use rustc_hash::FxHashMap;
 
-use smtlib::Storage;
 use smtlib::prelude::*;
-use smtlib::terms::Const;
 
 // The actual code
 
@@ -81,141 +75,6 @@ pub enum CondCode {
     Gt,
     /// Signed less than or equal - Z set OR N not equal to V
     Le,
-}
-
-#[derive(Clone, Copy, Debug, Default, PartialEq, Eq, Hash)]
-pub struct Flags<B = bool> {
-    pub z: B,
-    pub n: B,
-    pub c: B,
-    pub v: B,
-}
-
-impl<'st, B: Copy + Into<SmtBool<'st>>> BoolEq<SmtBool<'st>> for Flags<B> {
-    fn eq(&self, other: &Self) -> SmtBool<'st> {
-        self.z.into().eq(&other.z.into())
-            & self.n.into().eq(&other.n.into())
-            & self.c.into().eq(&other.c.into())
-            & self.v.into().eq(&other.v.into())
-    }
-}
-
-impl<'st> From<Flags<bool>> for Flags<SmtBool<'st>> {
-    fn from(Flags { z, n, c, v }: Flags<bool>) -> Self {
-        Self {
-            z: Bool::from_bool(z),
-            n: Bool::from_bool(n),
-            c: Bool::from_bool(c),
-            v: Bool::from_bool(v),
-        }
-    }
-}
-
-impl<'st> From<Flags<Const<'st, SmtBool<'st>>>> for Flags<SmtBool<'st>> {
-    fn from(Flags { z, n, c, v }: Flags<Const<'st, SmtBool<'st>>>) -> Self {
-        Self {
-            z: z.into(),
-            n: n.into(),
-            c: c.into(),
-            v: v.into(),
-        }
-    }
-}
-
-impl<'st> Flags<SmtBool<'st>> {
-    fn update_from_add<W: SmtWord<'st>>(&mut self, op1: W, op2: W, enabled: SmtBool<'st>) {
-        let sum = op1 + op2;
-        self.z = enabled.if_then_else(sum.is_zero(), self.z);
-        self.n = enabled.if_then_else(sum.is_negative(), self.n);
-        self.c = enabled.if_then_else(sum.unsigned_lt(op1), self.c);
-        let both_positive = op1.is_positive() & op2.is_positive();
-        let both_negative = op1.is_negative() & op2.is_negative();
-        self.v = enabled.if_then_else(
-            (both_positive & sum.signed_lt(op1)) | (both_negative & sum.is_positive()),
-            self.v,
-        );
-    }
-
-    fn update_from_sub<W: SmtWord<'st>>(&mut self, op1: W, op2: W, enabled: SmtBool<'st>) {
-        let diff = op1.sub(op2);
-        self.z = enabled.if_then_else(diff.is_zero(), self.z);
-        self.n = enabled.if_then_else(diff.is_negative(), self.n);
-        self.c = enabled.if_then_else(op2.unsigned_le(op1), self.c);
-        let op1_positive = op1.is_positive();
-        let op2_negative = op2.is_negative();
-        let op1_negative = op1.is_negative();
-        let op2_positive = op2.is_positive();
-        self.v = enabled.if_then_else(
-            (op1_positive & op2_negative & diff.is_negative())
-                | (op1_negative & op2_positive & diff.is_positive()),
-            self.v,
-        );
-    }
-}
-
-impl Flags<bool> {
-    fn update_from_add<W: Word>(&mut self, op1: W, op2: W, enabled: bool) {
-        if !enabled {
-            return;
-        }
-        let sum = op1 + op2;
-        self.z = sum.is_zero();
-        self.n = sum.signed_negative();
-        self.c = sum < op1;
-        let both_positive = op1.signed_positive() && op2.signed_positive();
-        let both_negative = op1.signed_negative() && op2.signed_negative();
-        self.v = (both_positive && sum.signed_lt(op1)) || (both_negative && sum.signed_positive());
-    }
-
-    fn update_from_sub<W: Word>(&mut self, op1: W, op2: W, enabled: bool) {
-        if !enabled {
-            return;
-        }
-        let diff = op1 - op2;
-        self.z = diff.is_zero();
-        self.n = diff.signed_negative();
-        self.c = op1 >= op2;
-        let op1_positive = op1.signed_positive();
-        let op2_negative = op2.signed_negative();
-        let op1_negative = op1.signed_negative();
-        let op2_positive = op2.signed_positive();
-        self.v = (op1_positive && op2_negative && diff.signed_negative())
-            || (op1_negative && op2_positive && diff.signed_positive());
-    }
-
-    /// Contains all possible combinations of flags.
-    #[rustfmt::skip]
-    pub const ALL: [Flags; 16 /* 2^4 */] = [
-        Flags { z: false, n: false, c: false, v: false },
-        Flags { z: true,  n: false, c: false, v: false },
-        Flags { z: false, n: true,  c: false, v: false },
-        Flags { z: true,  n: true,  c: false, v: false },
-        Flags { z: false, n: false, c: true,  v: false },
-        Flags { z: true,  n: false, c: true,  v: false },
-        Flags { z: false, n: true,  c: true,  v: false },
-        Flags { z: true,  n: true,  c: true,  v: false },
-        Flags { z: false, n: false, c: false, v: true  },
-        Flags { z: true,  n: false, c: false, v: true  },
-        Flags { z: false, n: true,  c: false, v: true  },
-        Flags { z: true,  n: true,  c: false, v: true  },
-        Flags { z: false, n: false, c: true,  v: true  },
-        Flags { z: true,  n: false, c: true,  v: true  },
-        Flags { z: false, n: true,  c: true,  v: true  },
-        Flags { z: true,  n: true,  c: true,  v: true  },
-    ];
-}
-
-impl Display for Flags<bool> {
-    fn fmt(&self, f: &mut Formatter) -> fmt::Result {
-        let &Self { z, n, c, v } = self;
-        let s = |b, c: char| if b { c } else { '─' };
-        let w = |f: &mut Formatter, b, c| write!(f, "{}", s(b, c));
-        w(f, z, 'Z')?;
-        w(f, n, 'N')?;
-        w(f, c, 'C')?;
-        w(f, v, 'V')?;
-        Ok(())
-    }
 }
 
 impl CondCode {
@@ -411,242 +270,11 @@ impl<W: Word> Clone for Inst<W> {
     }
 }
 
-bitflags::bitflags! {
-    #[derive(Clone, Copy, Debug, Display, Default, PartialEq, Eq, Hash, Serialize, Deserialize)]
-    #[display("{}", Flags::from(*self))]
-    pub struct FlagsBitField: u8 {
-        /// Zero - is the result zero? Disregard overflow and carry.
-        const Z = 0b0001;
-        /// Negative - is the Msb set? Disregard overflow and carry.
-        const N = 0b0100;
-        /// Carry - on when unsigned addition overflows or unsigned subtraction doesn't underflow.
-        const C = 0b0010;
-        /// Overflow - on signed addition/subtraction, on when result is out of signed range.
-        const V = 0b1000;
-    }
-}
-
-impl From<Flags> for FlagsBitField {
-    #[rustfmt::skip]
-    fn from(Flags { z, n, c, v } : Flags) -> Self {
-        let mut flags = FlagsBitField::empty();
-        if z { flags |= FlagsBitField::Z; }
-        if n { flags |= FlagsBitField::N; }
-        if c { flags |= FlagsBitField::C; }
-        if v { flags |= FlagsBitField::V; }
-        flags
-    }
-}
-
-impl<B: Bool> From<FlagsBitField> for Flags<B> {
-    fn from(value: FlagsBitField) -> Self {
-        Self {
-            z: B::from_bool(value.contains(FlagsBitField::Z)),
-            n: B::from_bool(value.contains(FlagsBitField::N)),
-            c: B::from_bool(value.contains(FlagsBitField::C)),
-            v: B::from_bool(value.contains(FlagsBitField::V)),
-        }
-    }
-}
-
-#[cfg(test)]
-impl proptest::arbitrary::Arbitrary for FlagsBitField {
-    type Parameters = ();
-    type Strategy = proptest::strategy::BoxedStrategy<Self>;
-
-    #[rustfmt::skip]
-    fn arbitrary_with(_args: Self::Parameters) -> Self::Strategy {
-        use proptest::prelude::*;
-        (any::<bool>(), any::<bool>(), any::<bool>(), any::<bool>())
-            .prop_map(|(z, n, c, v)| {
-                let mut flags = FlagsBitField::empty();
-                if z { flags |= FlagsBitField::Z; }
-                if n { flags |= FlagsBitField::N; }
-                if c { flags |= FlagsBitField::C; }
-                if v { flags |= FlagsBitField::V; }
-                flags
-            })
-            .boxed()
-    }
-}
-
 // =========================================== State ==============================================
 
-#[derive(Clone, Debug, Default, PartialEq, Eq, Hash, Serialize, Deserialize)]
-pub struct State<W> {
-    /// This vector is always sorted by register. Registers that are zero are omitted.
-    /// TODO: Change this!
-    pub flags: FlagsBitField,
-    pub registers: [W; Register::COUNT as usize],
-}
+pub mod state;
 
-impl<W: Copy> State<W> {
-    pub fn get_register(&self, reg: Register) -> W {
-        self.registers[reg.0 as usize]
-    }
-    pub fn set_register(&mut self, reg: Register, x: W) {
-        self.registers[reg.0 as usize] = x;
-    }
-    pub fn get_flags(&self) -> FlagsBitField {
-        // self.flags.expect("Flags not set in state.")
-        self.flags
-    }
-    pub fn set_flags(&mut self, flags: FlagsBitField) {
-        self.flags = flags
-    }
-    /// Copies this state to another state object. Used to avoid clones, that in a loop, can
-    /// allocate more.
-    #[inline]
-    pub fn clone_to(&self, other: &mut Self) {
-        other.registers = self.registers;
-        other.flags = self.flags;
-    }
-    pub fn reduce<WSmall: Word>(&self, reducer: &mut Reducer<W, WSmall>) -> State<WSmall>
-        where W: Word
-    {
-        State {
-            registers: self
-                .registers
-                .map(|v| reducer.reduce(v, &Default::default())),
-            flags: self.flags,
-        }
-    }
-    #[inline]
-    pub fn clear(&mut self) where W: Default {
-        self.registers = [W::default(); _];
-        self.flags = FlagsBitField::empty();
-    }
-
-    pub fn all_each(ei: &EnumerationInfoOptions<Register>, mut f: impl FnMut(&Self))
-        where W: All + Default, W::Iter: Clone
-    {
-        // fn or_none<T: Clone>(
-        //     i: impl Clone + Iterator<Item = T>,
-        // ) -> impl Clone + Iterator<Item = Option<T>> {
-        //     [None].into_iter().chain(i.map(Some))
-        // }
-
-        let registers = ei.into_iter().collect::<Box<[_]>>();
-        let reg_value_iter = registers.iter().map(|_| W::all()).collect::<Box<[_]>>();
-        let mut iter = PermutationIter::new(&reg_value_iter);
-        let mut state = State::default();
-        while let Some(reg_values) = iter.next_slice() {
-            state.clear();
-            for (r, &v) in registers.iter().cloned().zip(reg_values) {
-                state.set_register(r, v);
-            }
-            for flags in Flags::ALL {
-                state.set_flags(flags.into());
-                f(&state)
-            }
-        }
-    }
-}
-
-impl<W: Word, F, I> From<(F, I)> for State<W>
-where
-    F: Into<Flags>,
-    I: IntoIterator<Item = (Register, W)>,
-{
-    fn from((f, i): (F, I)) -> Self {
-        let mut ret = Self::default();
-        // Flags
-        ret.set_flags(f.into().into());
-        // Registers
-        for (r, v) in i {
-            ret.set_register(r, v);
-        }
-        ret
-    }
-}
-
-use std::fmt::{self, Display, Formatter};
-impl<W: Word> Display for State<W> {
-    fn fmt(&self, f: &mut Formatter) -> fmt::Result {
-        write!(f, "{}", self.get_flags())?;
-        for r in Register::all() {
-            let v = self.get_register(r);
-            if !v.is_zero() {
-                write!(f, " {r}={v:>2}")?;
-            }
-        }
-        Ok(())
-    }
-}
-
-impl<W: Word> collect_registers::State<W> for State<W> {
-    fn registers(&self) -> impl Iterator<Item = (Register, W)> {
-        Register::all().filter_map(|r| {
-            if !self.get_register(r).is_zero() {
-                Some((r, self.get_register(r)))
-            } else {
-                None
-            }
-        })
-    }
-}
-
-impl<W: Word> oracle::test_cases::State for State<W> {
-    fn clone_to(&self, output: &mut Self) {
-        self.clone_to(output);
-    }
-}
-
-#[derive(Clone, Copy, Debug)]
-pub struct StateVars<'st, W> {
-    pub registers: [Const<'st, W>; Register::COUNT as usize],
-    pub flags: Flags<Const<'st, SmtBool<'st>>>,
-}
-
-impl<'st, W: SmtWord<'st>> StateVars<'st, W> {
-    pub fn new(st: &'st Storage, name: &str) -> Self {
-        Self {
-            registers: std::array::from_fn(|i| {
-                W::new_const(st, &format!("{name}_r{i}"))
-            }),
-            flags: Flags::new(st, name),
-        }
-    }
-}
-
-impl<'st> Flags<Const<'st, SmtBool<'st>>> {
-    pub fn new(st: &'st Storage, name: &str) -> Self {
-        Self {
-            z: SmtBool::new_const(st, &format!("{name}_z")),
-            n: SmtBool::new_const(st, &format!("{name}_n")),
-            c: SmtBool::new_const(st, &format!("{name}_c")),
-            v: SmtBool::new_const(st, &format!("{name}_v")),
-        }
-    }
-}
-
-// TODO: Unify with [State]
-#[derive(Clone, Copy, Debug)]
-pub struct SymbolicState<'st, W> {
-    pub registers: [W; Register::COUNT as usize],
-    pub flags: Flags<SmtBool<'st>>,
-    // TODO: Live mask
-}
-
-impl<'st, W: SmtWord<'st>> From<StateVars<'st, W>> for SymbolicState<'st, W> {
-    fn from(value: StateVars<'st, W>) -> Self {
-        Self {
-            registers: value.registers.map(Into::into),
-            flags: value.flags.into(),
-        }
-    }
-}
-
-impl<'st, W: SmtWord<'st>> SymbolicState<'st, W> {
-    pub fn eq(&self, other: Self) -> SmtBool<'st> {
-        let regs = self.registers.iter().zip(other.registers);
-        let regs_eq = regs
-            .map(|(ra, rb)| ra._eq(rb))
-            .reduce(|b1, b2| b1 & b2)
-            .unwrap();
-        regs_eq & self.flags.eq(&other.flags)
-    }
-}
+pub use state::{Flags, FlagsBitField, State, StateVars, SymbolicState};
 
 fn run_instruction<W: Word>(inst: &Inst<W>, state: &mut State<W>) {
     /// Get a register value.
@@ -674,7 +302,9 @@ fn run_instruction<W: Word>(inst: &Inst<W>, state: &mut State<W>) {
     }
     /// Get an immediate value.
     macro_rules! imm {
-        ($i:literal) => { inst.args[$i] };
+        ($i:literal) => {
+            inst.args[$i]
+        };
     }
 
     // Skip the instruction if it is skipped by the flags.
@@ -711,7 +341,10 @@ fn run_instruction<W: Word>(inst: &Inst<W>, state: &mut State<W>) {
     }
 }
 
-fn run_instruction_symbolic<'st, W: Word>(inst: &Inst<W>, state: &mut SymbolicState<'st, W::SmtWord<'st>>) {
+fn run_instruction_symbolic<'st, W: Word>(
+    inst: &Inst<W>,
+    state: &mut SymbolicState<'st, W::SmtWord<'st>>,
+) {
     let enabled = inst.cond_code.check(state.flags);
 
     /// Get a register value.
@@ -785,7 +418,7 @@ impl<W: Word> BackwardMap<W> {
     pub fn new(registers: &[Register]) -> std::io::Result<Self>
     where
         Self: Serialize + for<'a> Deserialize<'a>,
-        <W as All>::Iter: Clone
+        <W as All>::Iter: Clone,
     {
         let file_path = std::path::Path::new(".").join(Self::file_name(registers));
         if file_path.exists() {
@@ -835,7 +468,10 @@ impl<W: Word> BackwardMap<W> {
     }
 
     /// Build from scratch a new backwards behavior map.
-    pub fn new_recalculate(registers: &[Register]) -> Self where <W as All>::Iter: Clone {
+    pub fn new_recalculate(registers: &[Register]) -> Self
+    where
+        <W as All>::Iter: Clone,
+    {
         let mut ret = FxHashMap::default();
         let mut i = 0;
         let ei = EnumerationInfoOptions::Limited(registers);
@@ -990,11 +626,14 @@ impl<W: Display> Display for Inst<W> {
         if *cond_code == CondCode::Al {
             write!(f, "{op_code} {}, {}, {}", args[0], args[1], args[2])
         } else {
-            write!(f, "{op_code}{cond_code} {}, {}, {}", args[0], args[1], args[2])
+            write!(
+                f,
+                "{op_code}{cond_code} {}, {}, {}",
+                args[0], args[1], args[2]
+            )
         }
     }
 }
-
 
 /// A macro to create an instruction more easily.
 #[macro_export]
