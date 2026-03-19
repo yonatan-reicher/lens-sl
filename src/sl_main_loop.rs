1a67b1d5e6e50e429237956db5d36175bdc07cca
Jonathan Reicher <yony252525@gmail.com>
2026-03-19 05:42:31 -0400

    update sl_main_loop.rs

--- src/sl_main_loop.rs
+++ src/sl_main_loop.rs
@@ -1,60 +1,54 @@
 use crate::all::All;
-use crate::arm::state::{BitMask, Flags, FlagsBitField, Mask, State};
+use crate::arm::state::{BitMask, Flags, FlagsBitField, Mask, Masked as MaskedState, State};
 use crate::arm::{Inst, Register};
 use crate::enumerate::{EnumerationInfo, EnumerationInfoOptions, Enumerator};
 use crate::graph::Graph;
-use crate::programs::Programs;
+use crate::oracle::Oracle;
+use crate::reduce_bit_width::Reducer;
 use crate::tui::TuiHook;
 use crate::word::prelude::*;
 use functionality::Mutate;
+use std::rc::Rc;
 
-#[derive(Clone, Copy, Default, Debug, PartialEq, Eq)]
-struct Masked<W> {
-    state: State<W>,
-    mask: BitMask,
+#[derive(Debug, Default)]
+struct Effect<W> {
+    input: MaskedState<W>,
+    output: MaskedState<W>,
 }
 
-#[derive(Default)]
-pub struct Bank<W> {
-    vec: Vec<Entry<W>>,
-}
+type Programs<W> = Rc<crate::programs::Programs<Inst<W>>>;
 
-#[derive(Clone, Default, Debug, PartialEq, Eq)]
-pub struct Entry<W> {
-    input: Masked<W>,   // TODO be a vector
-    prog: Vec<Inst<W>>, // A class
-    output: Masked<W>,
-}
-
-impl<W, M: Into<BitMask>> From<(State<W>, M, Vec<Inst<W>>, State<W>, M)> for Entry<W> {
-    fn from((i, i_m, p, o, o_m): (State<W>, M, Vec<Inst<W>>, State<W>, M)) -> Self {
-        Entry {
-            input: Masked {
-                state: i,
-                mask: i_m.into(),
-            },
-            prog: p,
-            output: Masked {
-                state: o,
-                mask: o_m.into(),
-            },
-        }
-    }
-}
+type Bank<W> = crate::bank::Bank<Effect<W>, Programs<W>>;
 
 // === Algorithm ===
 
 pub fn optimize<WBig: Word, W: Word>(
-    program: &[Inst<W>],
+    program: &[Inst<WBig>],
     examples: &[&[(Register, WBig)]],
-    tui: &impl for<'g> TuiHook<&'g Graph<State<W>, Programs<Inst<W>>>, State<W>>,
+    tui: &impl for<'g> TuiHook<&'g Graph<State<W>, Programs<W>>, State<W>>,
 ) -> Option<Vec<Inst<W>>>
 where
     <W as All>::Iter: Clone,
 {
-    let mut bank = Bank::default();
+    let mut bank = Bank::<W>::default();
+    let mut counter_examples = vec![];
 
-    let initial_state = todo!();
+    let mut reducer = Reducer::default();
+    let reduced_program = program.iter().map(|i| i.reduce(&mut reducer)).collect();
+    let mut oracle = crate::oracle::SmtOracle::new(reduced_program);
+
+    // Check the empty program. Obviously, this probably fails, but a. It might not, and b. We need
+    // it to generate the first counter example.
+    let initial_state: State<W> = match oracle.check_program(&[]) {
+        Ok(()) => return Some(vec![]),
+        Err(ce) => {
+            counter_examples.push(ce);
+            ce.0
+        }
+    };
+
+    // Initial the bank with our atomic single-instruction programs, on the initial and final
+    // states of our lonely counter-example.
     let ei = EnumerationInfo {
         registers: EnumerationInfoOptions::Limited(&[
             Register(0),
@@ -70,16 +64,12 @@ where
             Register(10),
             Register(11),
         ]),
-        immediates: EnumerationInfoOptions::Unlimited,
+        immediates: EnumerationInfoOptions::<W>::Unlimited,
     };
     for inst in Enumerator::new().into_iter(&ei) {
-        let 
+        inst.read_mask();
+        // find 0- ~4 registers that
     }
-
-
-
-
-
 
     /*
     // Initialize with the empty program in all sorts of forms
@@ -151,49 +141,8 @@ where
 
     println!("Reached end!");
 
-    println!("{}", bank.vec.len());
+    println!("{}", bank.n_effects());
     None
-}
-
-// TODO: Move to state.rs
-use std::ops::{BitAnd, BitOr};
-impl<W: Copy + Default> BitAnd<Mask> for Masked<W> {
-    type Output = Self;
-    fn bitand(self, mask: Mask) -> Self {
-        Self {
-            mask: self.mask & mask.into(),
-            state: self.state.mask(mask),
-            // mask mask mask mask...
-        }
-    }
-}
-impl<W: Copy + Default> BitOr for Masked<W> {
-    type Output = Self;
-    fn bitor(self, other: Self) -> Self {
-        // self takes priority
-        let mask = self.mask | other.mask;
-        // Convert the bit-masks to real masks
-        let (self_mask, other_mask) = (Mask::from(self.mask), Mask::from(other.mask));
-        let state = State {
-            flags: if self_mask.flags {
-                self.state.flags
-            } else if other_mask.flags {
-                other.state.flags
-            } else {
-                FlagsBitField::default()
-            },
-            registers: Register::ALL.map(|r| {
-                if self_mask[r] {
-                    self.state.get_register(r)
-                } else if other_mask[r] {
-                    other.state.get_register(r)
-                } else {
-                    W::default()
-                }
-            }),
-        };
-        Masked { state, mask }
-    }
 }
 
 // ======== Collect Children ======================================================================
@@ -202,44 +151,55 @@ impl<W: Copy + Default> BitOr for Masked<W> {
 /// arguments, which makes implementation and reasoning easier for us.
 fn collect_children<'a, W: Word>(
     bank: &'a Bank<W>,
-) -> impl Iterator<Item = (&'a Entry<W>, &'a Entry<W>)> {
+) -> impl Iterator<
+    Item = (
+        &'a Effect<W>,
+        &'a Programs<W>,
+        &'a Effect<W>,
+        &'a Programs<W>,
+    ),
+> {
     I {
         bank,
-        indices: (0, 0),
+        iters: (bank.iter().fuse().peekable(), bank.iter()),
     }
 }
 
+use std::iter::{Fuse, Peekable};
 struct I<'a, W: Word> {
     bank: &'a Bank<W>,
-    indices: (usize, usize),
+    iters: (
+        Peekable<Fuse<crate::bank::Iter<'a, Effect<W>, Programs<W>>>>,
+        crate::bank::Iter<'a, Effect<W>, Programs<W>>,
+    ),
 }
 
 impl<'a, W: Word> Iterator for I<'a, W> {
-    type Item = (&'a Entry<W>, &'a Entry<W>);
+    type Item = (
+        &'a Effect<W>,
+        &'a Programs<W>,
+        &'a Effect<W>,
+        &'a Programs<W>,
+    );
     fn next(&mut self) -> Option<Self::Item> {
-        // Check if the second index is done
-        if !(self.indices.1 < self.bank.vec.len()) {
-            self.indices.1 = 0;
-            self.indices.0 += 1;
+        // Check if the second iterator is done,
+        let Some(b) = self.iters.1.next() else {
+            self.iters.1 = self.bank.iter();
+            self.iters.0.next();
             return self.next();
-        }
-        // Check if the first index is done
-        if !(self.indices.0 < self.bank.vec.len()) {
-            return None;
-        }
-        let a = &self.bank.vec[self.indices.0];
-        let b = &self.bank.vec[self.indices.1];
+        };
+        // then check the first!
+        // Notice that if the first iterator is done, we
+        let a = self.iters.0.peek()?;
         // Check frame rule!
         // This marks the state that appears in the output of the first program and the input of
         // the first.
-        let conflict_mask = Mask::from(a.output.mask & b.input.mask);
-        if a.output.state.mask(conflict_mask) != b.input.state.mask(conflict_mask) {
-            self.indices.1 += 1;
+        let conflict_mask = Mask::from(a.0.output.mask() & b.0.input.mask());
+        if a.0.output.state().mask(conflict_mask) != b.0.input.state().mask(conflict_mask) {
             return self.next();
         }
         // Frame rule succeeds!
-        self.indices.1 += 1;
-        Some((a, b))
+        Some((a.0, a.1, b.0, b.1))
     }
 }
 
@@ -247,14 +207,14 @@ impl<'a, W: Word> Iterator for I<'a, W> {
 
 /// This [eval] is the 'Eval' from Sobeq, which takes children and a constructor and combined them
 /// together. Here the constructor is always concatenation.
-fn eval<W: Word>(a: &Entry<W>, b: &Entry<W>) -> Entry<W> {
-    let conflict_mask = a.output.mask & b.input.mask;
+fn eval<W: Word>(
+    a: (&Effect<W>, Programs<W>),
+    b: (&Effect<W>, Programs<W>),
+) -> (Effect<W>, Programs<W>) {
+    let conflict_mask = a.0.output.mask() & b.0.input.mask();
     // We need to mask the second's inputs which are fed by the outputs of the first.
-    let input = a.input | (b.input & (!conflict_mask).into());
-    let output = b.output | a.output;
-    Entry {
-        input,
-        prog: a.prog.clone().mutate(|v| v.extend_from_slice(&b.prog)),
-        output,
-    }
+    let input = a.0.input | (b.0.input & (!conflict_mask).into());
+    let output = b.0.output | a.0.output;
+    let prog = a.1.mutate(|p| Rc::make_mut(p).extend(&b.1));
+    (Effect { input, output }, prog)
 }
