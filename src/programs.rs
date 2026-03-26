e4343a5095d8133f2ee56a4b529197cd4f10b94f
Jonathan Reicher <yony252525@gmail.com>
2026-03-26 01:31:45 -0400

    update expand

--- src/programs.rs
+++ src/programs.rs
@@ -17,99 +17,33 @@ pub struct Programs<I>(Inner<I>);
 #[derive(Clone, Debug)]
 enum Inner<I> {
     /// A set containing only the empty program.
+    Empty,
     EmptyProgram,
-    Concat(Rc<(Programs<I>, I)>),
-    #[allow(unused)]
-    ConcatsMap(Rc<FxHashMap<I, Programs<I>>>),
-    /// A set given by a vector of concatenations.
-    ConcatsVec(Rc<Vec<(Programs<I>, I)>>),
-    // Add an option for an 'extend', i.e., that one or this one, something like that. Or
-    // maybe a linked list or who knows what.
+    Inst(I),
+    Concat(Rc<(Programs<I>, Programs<I>)>),
+    Extend(Rc<(Programs<I>, Programs<I>)>),
 }
 
 impl<I> Programs<I> {
     pub fn empty() -> Self {
-        Self(Inner::ConcatsVec(Rc::new(vec![])))
+        Self(Inner::Empty)
     }
 
     pub fn empty_program() -> Self {
         Self(Inner::EmptyProgram)
     }
 
-    pub fn concat(self, inst: I) -> Self {
-        Self(Inner::Concat(Rc::new((self, inst))))
+    pub fn inst(i: I) -> Self {
+        Self(Inner::Inst(i))
     }
 
-    pub fn extend(&mut self, x: &Programs<I>)
-    where
-        I: Clone + Debug + Eq + Hash,
-    {
-        match &x.0 {
-            Inner::EmptyProgram => match &self.0 {
-                Inner::EmptyProgram => (),
-                Inner::ConcatsVec(v) if v.is_empty() => *self = Self(Inner::EmptyProgram),
-                Inner::ConcatsMap(m) if m.is_empty() => *self = Self(Inner::EmptyProgram),
-                _ => unreachable!(""),
-            },
-            Inner::Concat(rc) => self.extend_concat(&rc.0, &rc.1),
-            Inner::ConcatsVec(vec) => {
-                self.reserve(vec.len());
-                for (p, i) in vec.iter() {
-                    self.extend_concat(p, i);
-                }
-            }
-            Inner::ConcatsMap(map) => {
-                self.reserve(map.len());
-                for (i, p) in map.iter() {
-                    self.extend_concat(p, i);
-                }
-            }
-        }
+    pub fn concat(self, rhs: Self) -> Self {
+        Self(Inner::Concat(Rc::new((self, rhs))))
     }
 
-    fn reserve(&mut self, n: usize)
-    where
-        I: Clone + Eq + Hash,
-    {
-        match &mut self.0 {
-            Inner::EmptyProgram => (),
-            Inner::Concat(rc) => {
-                let mut vec = Vec::with_capacity(n + 1);
-                vec.push((rc.0.clone(), rc.1.clone()));
-                self.0 = Inner::ConcatsVec(Rc::new(vec));
-            }
-            Inner::ConcatsMap(map) => Rc::make_mut(map).reserve(n),
-            Inner::ConcatsVec(vec) => Rc::make_mut(vec).reserve(n),
-        }
-    }
-
-    fn extend_concat(&mut self, progs: &Self, inst: &I)
-    where
-        I: Clone + Debug + Eq + Hash,
-    {
-        use Inner::{Concat, ConcatsMap, ConcatsVec, EmptyProgram};
-        match &mut self.0 {
-            EmptyProgram => {
-                unreachable!(
-                    "The set of empty programs should never be extended with another set, because extending a set should only happen after expanding a program! and when all programs are expanded, they all should have at least one instruction. Was extended with Concat({progs:?}, {inst:?})"
-                );
-            }
-            Concat(..) => {
-                self.reserve(1);
-                self.extend_concat(progs, inst)
-            }
-            ConcatsMap(map) => {
-                if let Some(y) = Rc::make_mut(map).get_mut(inst) {
-                    // Now we can recurse
-                    y.extend(progs);
-                } else {
-                    // This is so much faster than the other branch! Why not always do this? This
-                    // has a memory cost.
-                    Rc::make_mut(map).insert(inst.clone(), progs.clone());
-                }
-            }
-            ConcatsVec(vec) => Rc::make_mut(vec).push((progs.clone(), inst.clone())),
-        }
+    pub fn extend(&mut self, x: Programs<I>) {
+        let this = std::mem::take(self);
+        *self = Self(Inner::Extend(Rc::new((this, x))));
     }
 
     // Get a single program from the collection, if there is any.
@@ -118,64 +52,57 @@ impl<I> Programs<I> {
         I: Clone,
     {
         match &self.0 {
+            Inner::Empty => None,
+            Inner::Inst(i) => Some(vec![i.clone()]),
             Inner::EmptyProgram => Some(vec![]),
-            Inner::Concat(rc) => {
-                let (p, inst) = &**rc;
-                let mut prog = p.sample()?;
-                prog.push(inst.clone());
-                Some(prog)
+            Inner::Extend(rc) => {
+                let (a, b) = &**rc;
+                a.sample().or_else(|| b.sample())
             }
-            Inner::ConcatsMap(map) => map.iter().next().and_then(|(i, p)| {
-                let mut prog = p.sample()?;
-                prog.push(i.clone());
-                Some(prog)
-            }),
-            Inner::ConcatsVec(vec) => vec.first().and_then(|(p, i)| {
-                let mut prog = p.sample()?;
-                prog.push(i.clone());
-                Some(prog)
-            }),
+            Inner::Concat(rc) => {
+                let (lhs, rhs) = &**rc;
+                let lhs = lhs.sample()?;
+                let mut rhs = rhs.sample()?;
+                Some(lhs.mutate(|l| l.append(&mut rhs)))
+            }
         }
     }
 
-    pub fn try_each<B, F>(&self, mut f: F) -> ControlFlow<B>
+    pub fn try_each<B>(
+        &self,
+        f: &mut (impl ?Sized + FnMut(Program<I>) -> ControlFlow<B>),
+    ) -> ControlFlow<B>
     where
         I: Clone,
-        F: FnMut(Program<I>) -> ControlFlow<B>,
     {
         use ControlFlow::Continue;
         match &self.0 {
+            Inner::Empty => Continue(()),
             Inner::EmptyProgram => f(vec![]),
+            Inner::Inst(inst) => f(vec![inst.clone()]),
+            Inner::Extend(rc) => {
+                let (a, b) = &**rc;
+                a.try_each(f)?;
+                b.try_each(f)?;
+                Continue(())
+            }
             Inner::Concat(rc) => {
-                let (p, i) = &**rc;
-                p.try_each::<B, Box<dyn FnMut(Program<I>) -> ControlFlow<B>>>(Box::new(
-                    |mut prog| {
-                        prog.push(i.clone());
-                        f(prog)
-                    },
-                ))
-            }
-            Inner::ConcatsMap(map) => {
-                for (y, x) in map.iter() {
-                    x.try_each::<B, Box<dyn FnMut(Program<I>) -> ControlFlow<B>>>(Box::new(
-                        |mut prog: Program<I>| {
-                            prog.push(y.clone());
-                            f(prog)
-                        },
-                    ))?;
-                }
-                Continue(())
-            }
-            Inner::ConcatsVec(vec) => {
-                for (x, y) in vec.iter() {
-                    x.try_each::<B, Box<dyn FnMut(Program<I>) -> ControlFlow<B>>>(Box::new(
-                        |mut prog: Program<I>| {
-                            prog.push(y.clone());
-                            f(prog)
-                        },
-                    ))?;
-                }
-                Continue(())
+                let (lhs, rhs) = &**rc;
+                // p.try_each::<B, Box<dyn FnMut(Program<I>) -> ControlFlow<B>>>(Box::new(
+                lhs.try_each(
+                    (&mut |mut lhs: Program<I>| {
+                        let len = lhs.len();
+                        rhs.try_each(
+                            (&mut |mut rhs| {
+                                lhs.append(&mut rhs);
+                                f(lhs.clone())?;
+                                lhs.truncate(len);
+                                Continue(())
+                            })
+                                as &mut dyn FnMut(Program<I>) -> ControlFlow<B>,
+                        )
+                    }) as &mut dyn FnMut(Program<I>) -> ControlFlow<B>,
+                )
             }
         }
     }
@@ -184,7 +111,7 @@ impl<I> Programs<I> {
     where
         I: Clone,
     {
-        let ret = self.try_each(|prog| {
+        let ret = self.try_each(&mut |prog| {
             f(prog);
             ControlFlow::Continue::<()>(())
         });
@@ -219,18 +146,20 @@ impl<I: Clone> From<Programs<I>> for Vec<Program<I>> {
 impl<I> Len for Programs<I> {
     fn len(&self) -> usize {
         match &self.0 {
+            Inner::Empty => 0,
             Inner::EmptyProgram => 1,
-            Inner::Concat(rc) => rc.0.len(),
-            Inner::ConcatsMap(map) => map.values().map(|p| p.len()).sum(),
-            Inner::ConcatsVec(vec) => vec.iter().map(|(p, _)| p.len()).sum(),
+            Inner::Inst(..) => 1,
+            Inner::Extend(rc) => rc.0.len() + rc.1.len(),
+            Inner::Concat(rc) => rc.0.len() * rc.1.len(),
         }
     }
     fn is_empty(&self) -> bool {
         match &self.0 {
+            Inner::Empty => true,
             Inner::EmptyProgram => false,
-            Inner::Concat(rc) => rc.0.is_empty(),
-            Inner::ConcatsMap(map) => map.values().all(|p| p.is_empty()),
-            Inner::ConcatsVec(vec) => vec.iter().all(|(p, _)| p.is_empty()),
+            Inner::Inst(..) => false,
+            Inner::Extend(rc) => rc.0.is_empty() && rc.1.is_empty(),
+            Inner::Concat(rc) => rc.0.is_empty() || rc.1.is_empty(),
         }
     }
 }
@@ -250,11 +179,17 @@ impl<I: std::fmt::Display + Clone> std::fmt::Display for Programs<I> {
     }
 }
 
+impl<I: Clone> From<I> for Programs<I> {
+    fn from(i: I) -> Self {
+        Self::inst(i)
+    }
+}
+
 impl<I: Clone> FromIterator<I> for Programs<I> {
     fn from_iter<T: IntoIterator<Item = I>>(iter: T) -> Self {
         let mut ret = Programs::empty_program();
         for i in iter {
-            ret = ret.concat(i);
+            ret = ret.concat(Self::inst(i));
         }
         ret
     }
@@ -263,13 +198,14 @@ impl<I: Clone> FromIterator<I> for Programs<I> {
 impl<I: Clone + Debug + Eq + Hash> Extend<Programs<I>> for Programs<I> {
     fn extend<It: IntoIterator<Item = Self>>(&mut self, iter: It) {
         for p in iter {
-            self.extend(&p);
+            self.extend(p);
         }
     }
 }
 
 // -------------- tests ---------------------
 
+use functionality::Mutate;
 #[cfg(test)]
 use proptest::prelude::*;
 use rustc_hash::FxHashMap;
@@ -283,29 +219,25 @@ where
     type Parameters = ();
 
     fn arbitrary_with(_args: Self::Parameters) -> Self::Strategy {
-        let empty_program = Just(Self::empty_program());
-        empty_program
-            .prop_recursive(
-                8,  // maximum depth
-                40, // total size
-                5,  // amount of elements in a branch
-                |inner| {
-                    prop_oneof![
-                        // Concat
-                        (inner.clone(), any::<I>()).prop_map(|(p, i)| p.concat(i)),
-                        // Concats
-                        prop::collection::vec((inner.clone(), any::<I>()), 1..10).prop_map(|vec| {
-                            let concats = vec.into_iter().map(|(p, i)| (p, i)).collect();
-                            Self(Inner::ConcatsVec(Rc::new(concats)))
-                        }),
-                        prop::collection::vec((inner.clone(), any::<I>()), 1..10).prop_map(|vec| {
-                            let concats = vec.into_iter().map(|(p, i)| (i, p)).collect();
-                            Self(Inner::ConcatsMap(Rc::new(concats)))
-                        }),
-                    ]
-                },
-            )
-            .boxed()
+        let base = prop_oneof![
+            Just(Self::empty()),
+            Just(Self::empty_program()),
+            any::<I>().prop_map(Self::inst),
+        ];
+        base.prop_recursive(
+            8,  // maximum depth
+            40, // total size
+            5,  // amount of elements in a branch
+            |inner| {
+                prop_oneof![
+                    // Concat
+                    (inner.clone(), inner.clone()).prop_map(|(a, b)| a.concat(b)),
+                    // Extend
+                    (inner.clone(), inner.clone()).prop_map(|(a, b)| a.mutate(|a| a.extend(b)))
+                ]
+            },
+        )
+        .boxed()
     }
 
     type Strategy = BoxedStrategy<Self>;
