81af0242b7a84f2f6247027ff588754c6a80c7a0
Jonathan Reicher <yony252525@gmail.com>
2026-04-14 21:20:53 +0300

    add zoom axes

--- plot.py
+++ plot.py
@@ -10,22 +10,30 @@ sl = p.read_csv('sl.csv').replace('timeout', '11')
 
 time_column = 'time(seconds/timeout)'
 
-fig, ax = plt.subplots()
-ax.set_title('benchmark execution times (seconds)\nLower is better')
-ax.set_xlabel('lens')
-ax.set_ylabel('sl')
-ax.scatter(
-    lens[time_column].astype(float),
-    sl[time_column].astype(float),
-    s=100,
-    label=lens['name'],
-    color='red',
-    alpha=0.3,
-    edgecolors='none',
-)
-ax.axline((0, 0), (11, 11))
+fig, (ax_left, ax_right) = plt.subplots(1, 2)
+lim = 0.3
+ax_left.set_title('benchmark execution times (seconds)\nLower is better')
+ax_right.set_title('benchmark execution times (seconds [Zoomed in])\nLower is better')
+ax_right.set_xlim(0, lim)
+ax_right.set_ylim(0, lim)
 
-ax.set_xscale('log')
-ax.set_yscale('log')
+xs = lens[time_column].astype(float)
+ys = sl[time_column].astype(float)
+for ax in (ax_left, ax_right):
+    ax.set_xlabel('lens')
+    ax.set_ylabel('sl')
+    ax.scatter(
+        xs,
+        ys,
+        s=100,
+        label=lens['name'],
+        color='red',
+        alpha=0.3,
+        edgecolors='none',
+    )
+    ax.axline((0, 0), (11, 11))
+
+    for i, (x, y) in enumerate(zip(xs, ys)):
+        ax.annotate(lens['name'][i], xy=(x, y))
 
 plt.show()
