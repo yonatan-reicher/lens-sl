e320000076fa621704a281cbceccb2c86070945d
Jonathan Reicher <yony252525@gmail.com>
2026-04-19 11:28:51 +0300

    add some testst (failing!)

--- src/arm.rs
+++ src/arm.rs
@@ -1033,4 +1033,54 @@ mod tests {
         prop_assert_eq!(r, inst.potential_read_mask());
         prop_assert_eq!(w, inst.potential_write_mask());
     }
+
+    static BM: std::sync::LazyLock<BackwardMap<Word4>> = std::sync::LazyLock::new(|| {
+        BackwardMap::new(&[Register(0), Register(1), Register(2)]).unwrap()
+    });
+
+    #[property_test]
+    fn run_backward_masked_same_as_run_backward(
+        inst: Inst<Word4>,
+        state: State<Word4>,
+        mask: Mask,
+    ) {
+        let state = state.masked(mask);
+        println!("------------------------------");
+        println!("inst {inst}  state {state}");
+        let inputs = inst
+            .run_backward(*state.state(), &BM)
+            .into_iter()
+            .collect::<Vec<_>>();
+        println!("inputs");
+        inputs.iter().for_each(|i| println!("  {i}"));
+        for inp in inst.run_backward_masked(state, &BM) {
+            prop_assert!(
+                inputs.contains(inp.state()),
+                "{inp} was not contained in the inputs."
+            );
+        }
+    }
+
+    #[property_test]
+    fn run_backward_masked_no_missing_inputs(
+        inst: Inst<Word4>,
+        state: State<Word4>,
+        mask: Mask,
+    ) {
+        let state = state.masked(mask);
+        println!("------------------------------");
+        println!("inst {inst}  state {state}");
+        let inputs = inst
+            .run_backward(*state.state(), &BM)
+            .into_iter()
+            .collect::<Vec<_>>();
+        println!("inputs");
+        inputs.iter().for_each(|i| println!("  {i}"));
+        let masked_inputs = inst.
+            run_backward_masked(state, &BM)
+            .collect::<Vec<_>>();
+        println!("masked inputs");
+        masked_inputs.iter().for_each(|i| println!("  {i}"));
+        prop_assert_eq!(inst.run_backward_masked(state, &BM).count(), inputs.len());
+    }
 }
