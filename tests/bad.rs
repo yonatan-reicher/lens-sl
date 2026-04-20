51671e30de919352dc2be33ad680d5c0df260951
Jonathan Reicher <yony252525@gmail.com>
2026-04-20 13:08:38 +0300

    remove out-of-date test

--- tests/bad.rs
+++ tests/bad.rs
@@ -21,31 +21,3 @@ fn bad_case() {
     .unwrap();
     assert_eq!(p, None);
 }
-
-#[test]
-#[ignore]
-fn bad_case_2() {
-    let p = optimize::<Word64, Word4>(
-        Config {
-            algorithm: Algorithm::Lens,
-            program: &[
-                inst!(AddI, 0, 0, 5),
-                inst!(AddI Eq, 1, 0, 1),
-                inst!(Mul Eq, 1, 0, 1),
-                inst!(Orr, 0, 0, 1),
-                inst!(AddI Eq, 1, 0, 1),
-            ],
-            ..Config::default()
-        },
-        &NoTui,
-    )
-    .unwrap();
-    assert_eq!(
-        p,
-        Some(vec![
-            inst!(AddI, 0, 0, 5),
-            inst!(AddI Eq, 1, 0, 1),
-            inst!(Orr, 0, 0, 1)
-        ])
-    );
-}
