737a917c36d742a2779249ee03e3966941909d77
Jonathan Reicher <yony252525@gmail.com>
2026-04-22 21:41:20 +0300

    remove tui.searching call

--- src/main_loop_sl.rs
+++ src/main_loop_sl.rs
@@ -245,7 +245,6 @@ impl<'a, WBig: Word + HasBitWord, W: Word + HasBitWord> Optimizer<'a, WBig, W> {
             self.tui.report_length(Direction::Forward, self.prefix_len);
             self.tui
                 .report_length(Direction::Backward, self.postfix_len);
-                self.tui.searching();
             while self.postfix_len + self.prefix_len < self.original_reduced.len() {
                 self.tui.progress(0, self.stats.n_instructions);
                 let direction = Direction::from_is_forward(
