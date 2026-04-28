82f799b19b913c89319cf823f463178936f0d43d
Jonathan Reicher <yony252525@gmail.com>
2026-04-28 10:56:53 +0300

    update insts_with_precondtion

--- src/main_loop_sl.rs
+++ src/main_loop_sl.rs
@@ -666,33 +666,55 @@ where
             })
         })
     }
-}
 
-/// Gets all instructions which have the same input
-/// Find all instructions (and their effects!) that can run from the current states.
-/// Instead of doing this by iterating all instructions, do this by intersection of equivalence
-/// classes that can run from the states.
-fn insts_with_precondtion<'a, W: Word + HasBitWord>(
-    ei: EnumerationInfo<'a, W>,
-    bank: &'a Bank<W>,
-    inputs: &'a [State<W>],
-    input_mask: Mask,
-) -> impl IntoIterator<Item = Inst<W>> + use<'a, W> {
-    let inp_registers = input_mask.registers().collect::<Box<[_]>>();
-    let evil_reference: &[_] = unsafe { &*std::ptr::from_ref(inp_registers.as_ref()) };
-    let mut iter = Inst::enumerate(EnumerationInfo {
-        // Limit only to registers relevant to the input.
-        inp_registers: EnumerationInfoOptions::Limited(evil_reference),
-        ..ei
-    })
-    .filter(move |inst| inst.potential_input_mask() == input_mask);
-    std::iter::from_fn(move || {
-        let ret = iter.next();
-        // Make sure this closure keeps the input registers allocation alive, so that the fact that
-        // this closure captures it is not optimized away and we get a use-after-free.
-        std::hint::black_box(&inp_registers);
-        ret
-    })
+    /// Gets all instructions which have the same input
+    /// Find all instructions (and their effects!) that can run from the current states.
+    /// Instead of doing this by iterating all instructions, do this by intersection of equivalence
+    /// classes that can run from the states.
+    fn insts_with_precondtion<'a>(
+        ei: EnumerationInfo<'a, W>,
+        bank: &'a Bank<W>,
+        inputs: &[State<W>],
+        input_mask: Mask,
+        insts_input_table: &'a InstInputTable<W>,
+    ) -> impl IntoIterator<Item = Inst<W>> + use<'a, W, WBig> {
+        insts_input_table
+            .get(input_mask.into_bit_mask())
+            .iter()
+            .cloned()
+
+        // let inp_registers = input_mask.registers().collect::<Box<[_]>>();
+        // let evil_reference: &[_] = unsafe { &*std::ptr::from_ref(inp_registers.as_ref()) };
+        // let mut iter = Inst::enumerate(EnumerationInfo {
+        //     // Limit only to registers relevant to the input.
+        //     inp_registers: EnumerationInfoOptions::Limited(evil_reference),
+        //     ..ei
+        // })
+        // .filter(move |inst| inst.potential_input_mask() == input_mask);
+        // std::iter::from_fn(move || {
+        //     let ret = iter.next();
+        //     // Make sure this closure keeps the input registers allocation alive, so that the fact that
+        //     // this closure captures it is not optimized away and we get a use-after-free.
+        //     std::hint::black_box(&inp_registers);
+        //     ret
+        // })
+
+        // inputs
+        //     .iter()
+        //     .map(|input| input.masked(input_mask))
+        //     .map(|sub_input| {
+        //         // For this state, return set of commands that can run from it.
+        //         bank.get(&sub_input)
+        //             .iter()
+        //             // union
+        //             .flat_map(|(_, set)| RefIter::new(set.borrow(), |x| *x))
+        //             .collect::<FxHashSet<_>>()
+        //     })
+        //     .collect::<Vec<_>>()
+        //     // Intersect!
+        //     .as_slice()
+        //     .pipe(|a| intersect_all(a.iter())) // TODO: Change this with a syntactic lookup
+    }
 }
 
 /// Gets instructions which have the same effect on the input state
