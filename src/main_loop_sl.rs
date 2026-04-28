3fe069decc4658fa683e646b20eec4b418065a63
Jonathan Reicher <yony252525@gmail.com>
2026-04-28 18:28:09 +0300

    larger classes first

--- src/main_loop_sl.rs
+++ src/main_loop_sl.rs
@@ -772,8 +772,8 @@ where
     ) -> impl Iterator<Item = (Vec<State<W>>, Programs<W>)> + '_ {
         frontier
             .drain()
-            // .sorted_by_key(|(_, progs)| usize::MAX - progs.len())
-            .sorted_by_key(|(_, progs)| progs.len())
+            .sorted_by_key(|(_, progs)| usize::MAX - progs.len())
+            // .sorted_by_key(|(_, progs)| progs.len())
     }
 
     fn decide_direction(&self) -> Direction {
