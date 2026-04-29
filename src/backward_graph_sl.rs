36dc1c3757523f53bbb9aaccd517fd72b7c342dc
Jonathan Reicher <yony252525@gmail.com>
2026-04-29 08:53:41 +0300

    format

--- src/backward_graph_sl.rs
+++ src/backward_graph_sl.rs
@@ -1,10 +1,10 @@
 use crate::len::Len;
 use crate::programs_sl::Programs;
-use std::ops::ControlFlow;
+use itertools::Itertools;
 use rustc_hash::FxHashMap;
 use std::fmt::Debug;
 use std::hash::Hash;
-use itertools::Itertools;
+use std::ops::ControlFlow;
 
 /// BackwardsMap[postfix-length][test-index][input] = set of programs, that postfix-length
 /// instructions before the end, on the given input, return output of the given test.
@@ -59,19 +59,28 @@ where
             return Continue(false);
         }
         let start = &self.g.longest().unwrap();
-        let sets =
-            self.inputs.iter().zip(start.iter()).map(|(s, map)| map[s].clone())
+        let sets = self
+            .inputs
+            .iter()
+            .zip(start.iter())
+            .map(|(s, map)| map[s].clone())
             .sorted_by_key(|s| s.len())
             .collect_vec();
         let Some((i, smallest)) = sets.iter().enumerate().min_by_key(|(_, s)| s.len()) else {
             return Continue(false);
         };
-        smallest.try_each_reversed(|p| {
-            if sets.iter().enumerate().all(|(j, ps)| j == i || ps.contains(p.iter().cloned())) {
-                f(p)?;
-            }
-            Continue(())
-        }).map_continue(|()| true)
+        smallest
+            .try_each_reversed(|p| {
+                if sets
+                    .iter()
+                    .enumerate()
+                    .all(|(j, ps)| j == i || ps.contains(p.iter().cloned()))
+                {
+                    f(p)?;
+                }
+                Continue(())
+            })
+            .map_continue(|()| true)
     }
 
     pub fn try_descend(&mut self, state: S) -> Result<(), ()> {
